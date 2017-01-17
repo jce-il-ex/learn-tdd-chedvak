f29f941928531b403a9e34539c4a4a14c522d41a
Tmura <Tmura>
2017-01-17 18:56:26 +0200

    GREEN

--- cash.js
+++ cash.js
@@ -1,6 +1,6 @@
-/*var C = {};                    // C Object simplifies exporting the module
+var C = {};                    // C Object simplifies exporting the module
 C.getChange = function () {    // enough to satisfy the test
     'use strict';
     return true;               // also passes JSLint
 };
-module.exports = C;*/
+module.exports = C;
